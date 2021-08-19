76e3b0439bcd5a123e1d7052b392a1f8d8173130
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-08-19 19:05:11 +0200

    initial

---
+++ UD24Ejercicio01/src/main/java/com/employee/ud24/Ud24Ejercicio01Application.java
@@ -0,0 +1,13 @@
+package com.employee.ud24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud24Ejercicio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud24Ejercicio01Application.class, args);
+	}
+
+}
---
+++ UD24Ejercicio01/src/test/java/com/employee/ud24/Ud24Ejercicio01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.ud24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud24Ejercicio01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
